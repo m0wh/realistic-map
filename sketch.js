d09b39bce74423e3c9f07b7dbe1e98fa2d8c19cb
Malo Widerspach <malo.dewiderspach@gmail.com>
2018-07-20 17:10:20 +0200

    Fix reverse elevation bug

--- sketch.js
+++ sketch.js
@@ -15,33 +15,51 @@ function setup() {
 }
 
 function draw() {
+	let higherVal = 0;
+	let higherPt = [0, 0];
 	for (let x = 0; x < width; x+=precision) {
 		for (let y = 0; y < height; y+=precision) {
 		
-			elevation = map(noise(x*(1/scale), y*(1/scale)), 0, 1, 0, 1);
+			elevation = getElevation(x, y);
 
 			if (elevation <= .5) {
 				elevation /= 2; // océans profonds
 			}
 
 			if (x > 0 && y > 0 && x < width && y < height) {
-				topLeftElev = map(noise((x-precision)*(1/scale), (y-precision)*(1/scale)), 0, 1, 0, 1);
+				topLeftElev = getElevation(x - precision, y - precision);
 				if (topLeftElev <= .5) {
-					topLeftElev *= .97; // océans profonds
+					topLeftElev *= .97;
 				}
-				bottomRightElev = map(noise((x+precision)*(1/scale), (y+precision)*(1/scale)), 0, 1, 0, 1);
+				bottomRightElev = getElevation(x + precision, y + precision);
 				if (bottomRightElev <= .5) {
-					bottomRightElev *= .97; // océans profonds
+					bottomRightElev *= .97;
 				}
-				normal = (bottomRightElev - topLeftElev) / precision;
+				normal = (topLeftElev - bottomRightElev) / precision;
 			} else {
 				normal = 0;
 			}
-
-			color = [elevation, .5+elevation/2, 1-elevation];
+			
+			color = mapColorFunctions(elevation);
 			fill(...color); rect(x, y, precision, precision);
 			fill(0, 0, 0, normal*10); rect(x, y, precision, precision); // normal map
 
+			if (elevation > higherVal) {
+				higherVal = elevation;
+				higherPt = {x: x, y: y};
+			}
+
 		}
 	}
+}
+
+const getElevation = (x, y) => 1 - noise((x)*(1/scale), (y)*(1/scale));
+const limit = (val, min, max) => val >= max ? max : val <= min ? min : val;
+
+mapColorFunctions = (elev) => {
+	const h = 1- limit(elev*1.1, 0, .9);
+	const s = round(elev)/2+.5;
+	const l = .05+elev*.90;
+
+	return [h, s, l];
 }
