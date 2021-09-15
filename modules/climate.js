d5d2195f94fd1efad7be77375e6b53f6289b76f6
m0wh <malo.dewiderspach@gmail.com>
2021-09-15 17:32:33 +0200

    n token

--- modules/climate.js
+++ modules/climate.js
@@ -1,50 +1,45 @@
-function getClimate(elevation, moisture) {
+function getClimate(elevation, moisture, normal) {
 
-	const _elevation = limit(map(elevation, 0, 1, -1, 1), -1, .99);
-	const _moisture = limit(moisture, 0, .99);
+  const _elevation = limit(map(elevation, 0, 1, -1, 0.9), -1, .99);
+  const _moisture = limit(moisture, 0, .99);
 
-	const climateMap = [
-		/* Inspired by Whittaker diagram */
-		["SUBTROPICAL_DESERT", "GRASSLAND", "TUNDRA", "TROPICAL_SEASONAL_FOREST", "TROPICAL_SEASONAL_FOREST", "TROPICAL_RAIN_FOREST"],
-		["TEMPERATE_DESERT", "GRASSLAND", "GRASSLAND", "TEMPERATE_DECIDUOUS_FOREST", "TEMPERATE_DECIDUOUS_FOREST", "TEMPERATE_RAIN_FOREST"],
-		["TEMPERATE_DESERT", "TEMPERATE_DESERT", "SHRUBLAND", "SHRUBLAND", "TAIGA", "TAIGA"],
-		["SCORCHED", "BARE", "TUNDRA", "SNOW", "SNOW", "SNOW"]
-	];
+  const climateMap = [
+    /* Inspired by Whittaker diagram */
+    ["SUBTROPICAL_DESERT", "GRASSLAND", "TUNDRA", "TROPICAL_SEASONAL_FOREST", "TROPICAL_SEASONAL_FOREST", "TROPICAL_RAIN_FOREST"],
+    ["TEMPERATE_DESERT", "GRASSLAND", "GRASSLAND", "TEMPERATE_DECIDUOUS_FOREST", "TEMPERATE_DECIDUOUS_FOREST", "TEMPERATE_RAIN_FOREST"],
+    ["TEMPERATE_DESERT", "TEMPERATE_DESERT", "SHRUBLAND", "SHRUBLAND", "TAIGA", "TAIGA"],
+    ["SCORCHED", "BARE", "TUNDRA", "SNOW", "SNOW", "SNOW"]
+  ];
 
-	const colorMap = {
-		TEMPERATE_RAIN_FOREST: "#a4c4a8",
-		TEMPERATE_DECIDUOUS_FOREST: "#b4c9a9",
-		TROPICAL_SEASONAL_FOREST: "#a9cca4",
-		TROPICAL_RAIN_FOREST: "#9cbba9",
-		SNOW: "#f8f8f8",
-		SHRUBLAND: "#c4ccbb",
-		GRASSLAND: "#c4d4aa",
-		BARE: "#bbbbbb",
-		SCORCHED: "#999999",
-		TAIGA: "#ccd4bb",
-		TUNDRA: "#ddddbb",
-		TEMPERATE_DESERT: "#e4e8ca",
-		SUBTROPICAL_DESERT: "#e9ddc7",
+  const colorMap = {
+    TEMPERATE_RAIN_FOREST: "#a4c4a8",
+    TEMPERATE_DECIDUOUS_FOREST: "#b4c9a9",
+    TROPICAL_SEASONAL_FOREST: "#a9cca4",
+    TROPICAL_RAIN_FOREST: "#9cbba9",
+    SNOW: "#f8f8f8",
+    SHRUBLAND: "#c4ccbb",
+    GRASSLAND: "#c4d4aa",
+    BARE: "#bbbbbb",
+    SCORCHED: "#999999",
+    TAIGA: "#ccd4bb",
+    TUNDRA: "#ddddbb",
+    TEMPERATE_DESERT: "#e4e8ca",
+    SUBTROPICAL_DESERT: "#e9ddc7",
 
-		OCEAN: "#363661",
-		LAKE: "#557da6"
-	}
-	
-	return {
-		get color() {
-			try {
-				if (_elevation < 0) {
-					return colorMap['OCEAN'];
-				}
-				return colorMap[climateMap[floor(_elevation*4)][floor(_moisture*6)]]
-			} catch (e) {
-				console.log(_moisture);
-				// return ("#ffffff")
-			}
-		},
+    OCEAN: "#363661",
+    LAKE: "#557da6"
+  }
 
-		get name() {
-			return climateMap[floor(_elevation*4)][floor(_moisture*6)].toLowerCase
-		}
-	};
+  let finalColor
+  if (_elevation < 0) {
+    finalColor = color(colorMap['LAKE'])
+  } else {
+    const col = color(colorMap[climateMap[floor(_elevation*4)][floor(_moisture*6)]])
+    finalColor = color(`hsl(${Math.round(col._getHue())}, ${Math.round(col._getSaturation())}%, ${Math.round(col._getLightness() + normal)}%)`)
+  }
+  
+  return {
+    color: finalColor,
+    // name: climateMap[floor(_elevation*4)][floor(_moisture*6)].toLowerCase()
+  };
 }
--- sketch.js
+++ sketch.js
@@ -1,24 +1,41 @@
-const N = 2000; // number of Voronoi tile
-const NOISE_SCALE = 200;
+const n_id = Math.round(Math.random() * 8887) + 1
+let n_data
+let n_token
+let N // number of Voronoi tile
+let elevation_noise_scale
 
-let points;
-let voronoi;
-let diagram;
+let points
+let voronoi
+let diagram
+
+function preload () {
+  n_data = loadJSON('https://raw.githubusercontent.com/TSnark/n-project/main/data/n.json')
+}
 
 function setup() {
-  createCanvas(800, 600);
-  noLoop();
+  createCanvas(800, 600)
+  noLoop()
 
-  voronoi = new Voronoi();  
-  points = generatePoints(N, width, height, 5);
-  diagram = voronoi.compute(points, { xl: -2, xr: width+2, yt: -2, yb: height+2 });
+  n_token = n_data[n_id - 1][n_id.toString()]
+  console.table(n_token)
+  N = n_token.sum * n_token.sum
+  elevation_noise_scale = n_token.first * 100
+  moisture_noise_scale = n_token.second * 100
+
+  colorMode('HSL', 100)
+
+  voronoi = new Voronoi()  
+  points = generatePoints(N, width, height, 5)
+  diagram = voronoi.compute(points, { xl: -2, xr: width+2, yt: -2, yb: height+2 })
 }
 
 function draw() {
   diagram.cells.forEach(cell => {
-    cell.elevation = ridgenoise(cell.site.x/NOISE_SCALE, cell.site.y/NOISE_SCALE);
-    drawCell(cell);
-  });
+    cell.elevation = ridgenoise(cell.site.x / elevation_noise_scale, cell.site.y / elevation_noise_scale)
+    cell.moisture = ridgenoise(n_token.sum + cell.site.x / moisture_noise_scale, n_token.sum + cell.site.y / moisture_noise_scale)
+    cell.normal = (cell.elevation - ridgenoise((cell.site.x - 10) / elevation_noise_scale, (cell.site.y - 10) / elevation_noise_scale)) * 100
+    drawCell(cell)
+  })
 }
 
 /**
@@ -26,13 +43,15 @@ function draw() {
  * @param cell Voronoi.Cell object (from Javascript-Voronoi)
  */
 const drawCell = cell => {
-  // ellipse(cellCentroid(cell).x, cellCentroid(cell).y, 2, 2);
-  paint(cell.elevation * 255);
-  beginShape();
+  // ellipse(cellCentroid(cell).x, cellCentroid(cell).y, 2, 2)
+  const cl = getClimate(cell.elevation, cell.moisture, cell.normal)
+
+  paint(cl.color)
+  beginShape()
   cell.halfedges.forEach((h, i) => {
-    vertex(h.getStartpoint().x, h.getStartpoint().y);
-  });
-  endShape(cell.closeMe ? CLOSE : null);
+    vertex(h.getStartpoint().x, h.getStartpoint().y)
+  })
+  endShape(cell.closeMe ? CLOSE : null)
 }
 
 /**
@@ -43,16 +62,16 @@ const drawCell = cell => {
  * @param {number} iterations Number of Voronoi relaxing iterations
  */
 const generatePoints = (n, w, h, iterations = 2) => {
-  let pts = new Array(n).fill().map(p => createVector(random(w), random(h)));
-  const voro = new Voronoi();
+  let pts = new Array(n).fill().map(p => createVector(random(w), random(h)))
+  const voro = new Voronoi()
 
   for (let i = 0; i < iterations; i++) {
-    const cells = voro.compute(pts, { xl: -2, xr: w+2, yt: -2, yb: h+2 }).cells;
-    pts = cells.map( c => cellCentroid(c));
+    const cells = voro.compute(pts, { xl: -2, xr: w+2, yt: -2, yb: h+2 }).cells
+    pts = cells.map( c => cellCentroid(c))
   }
-  return pts;
+  return pts
 }
 
 const addIsland = () => {
-  console.log(diagram);
+  console.log(diagram)
 }
